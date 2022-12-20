317a1b9ce15ca88fdb2ceaca354fd232a4245f7e
yashgupta1299 <86516989+yashgupta1299@users.noreply.github.com>
2022-12-20 08:52:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const template = (temp, product) => {
+    let output = temp.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%PRODUCT-NAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+    if (!product.organic)
+        output = output.replace(/{%NOT-ORGANIC%}/g, 'not-organic');
+    return output;
+};
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const card_temp = fs.readFileSync(`${__dirname}/templates/card-temp.html`, 'utf-8');
+const overview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const product_temp = fs.readFileSync(`${__dirname}/templates/product-temp.html`, 'utf-8');
+
+// console.log(typeof(product_temp)); // string
+// console.log(card_temp);
+const server = http.createServer((req, res) => {
+    // const path = req.url;
+    const {query, pathname} = url.parse(req.url, true);
+    if (pathname=== '/' || pathname == '/overview')
+    {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const cardArray = dataObj.map(pro => template(card_temp, pro)).join('');
+        const output = overview.replace('{%CARD%}', cardArray);
+        res.end(output);
+    }
+    else if (pathname == '/product')
+    {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const output = template(product_temp, dataObj[query['id']]);
+        res.end(output);
+    }
+    else if (pathname == '/contacts')
+        res.end('contacts is here');
+    else if (pathname === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+    }
+    else {
+        res.writeHead(404, {
+            'content-type': 'text/html',
+            'my-header': 'hello'
+        });
+        res.end('<h1>page not here..</h1>')
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to request on port 8000');
+});
